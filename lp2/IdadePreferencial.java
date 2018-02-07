44cf3fa3fc5fbd7f65acc525eaf3fadc6b55f661
Bruno-Fernando <bruno.silva@ccc.ufcg.edu.br>
2018-02-07 12:42:51 -0300

    Add files via upload

---
+++ lp2/IdadePreferencial.java
@@ -0,0 +1,8 @@
+//Aluno: Bruno Fernando da Silva
+
+public class IdadePreferencial {
+	public static void main (String[] args) {
+		System.out.println("Bruno,18 anos, nao pode usar atendimento prefenrencial");
+	}
+
+}
---
+++ lp2/lab01/IdadePreferencialInteligente.java
@@ -0,0 +1,13 @@
+//Aluno: Bruno Fernando da Silva
+package lp2.lab01;
+public class IdadePreferencialInteligente {
+	public static void main (String[] args) {
+		int x = 2016 - 1998; 
+		if (x > 60){ 
+			System.out.println("Bruno de " + x + " pode usar caixa preferencial"); 
+		}else{ 
+			System.out.println("Bruno " + x + " nao pode usar caixa preferencial"); 
+		} 
+	}
+
+}
