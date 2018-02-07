f5c34633f507499dd50f5e8b3ff89bd2aeea9b6b
Bruno-Fernando <bruno.silva@ccc.ufcg.edu.br>
2018-02-07 12:47:19 -0300

    Add files via upload

---
+++ sp2fy/src/lp2/lab04/Musica.java
@@ -0,0 +1,16 @@
+package lp2.lab04;
+
+public class Musica {
+	private String titulo;
+	private int duracao;
+	private String genero;
+	
+	public Musica(String titulo, int duracao, String genero){
+		this.titulo = titulo;
+		this.duracao = duracao;
+		this.genero = genero;
+	}
+	
+	
+	
+}
